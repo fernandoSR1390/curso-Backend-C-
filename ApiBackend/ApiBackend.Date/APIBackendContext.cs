c1733e86678228e850d9093f28efc9fed56ce314
fernandoSR1390 <fernandosalinasromero04@gmail.com>
2023-07-13 11:06:26 -0400

    Subida principal APIBackendC#

---
+++ ApiBackend/ApiBackend.Date/APIBackendContext.cs
@@ -0,0 +1,21 @@
+﻿using ApiBackend.Shared;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiBackend.Date
+{
+    public class APIBackendContext : DbContext
+    {
+        public APIBackendContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
